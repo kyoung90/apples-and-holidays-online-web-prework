b5082f7adbf29704059c18d2ee3b446d9af56970
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 19:46:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    arr = key.split(" ")
+    arr = key.to_s.split(" ")
     arr.collect do |ele|
       ele.capitalize
     end
